2144768e34f60083b218e981f945fda8510cc53c
Sara Mazzoli <sara.mazoli8@studio.unibo.it>
2025-11-16 11:48:57 +0100

    svolto es 82

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/api/DeathNoteImpl.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/api/DeathNoteImpl.java
@@ -1,38 +1,130 @@
 package it.unibo.deathnote.api;
 
-public class DeathNoteImpl implements DeathNote{
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 
+/**
+ * Implementation of the DeathNote interface.
+ */
+public final class DeathNoteImpl implements DeathNote {
 
+    private static final long VALID_CAUSE_TIME = 40;
+    private static final long VALID_DETAILS_TIME = 6000 + VALID_CAUSE_TIME;
+    private final Map<String, Death> deathnote;
+    private String lastName;
+    private long lastNameTime;
+    private Death lastDeath;
 
-    public String getRule(int ruleNumber) {
-        throw new IllegalArgumentException("given rule number is smaller than 1 or larger than the number of rules");
+    /**
+     * a new empty deathnote.
+     */
+    public DeathNoteImpl() {
+        deathnote = new LinkedHashMap<>();
     }
 
-    public void writeName(String name) {
-        throw new NullPointerException("given name is null");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getRule(final int ruleNumber) {
+        if (ruleNumber <= 0 || ruleNumber > RULES.size()) {
+            throw new IllegalArgumentException("given rule number is smaller than 1 or larger than the number of rules");
+        }
+        return RULES.get(ruleNumber - 1);
     }
 
-    public boolean writeDeathCause(String cause) {
-        throw new IllegalStateException("there is no name written in this Death or the cause is null");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeName(final String name) {
+        Objects.requireNonNull(name, "name cannot be null");
+        lastDeath = new Death();
+        deathnote.put(name, lastDeath);
+        lastName = name;
+        lastNameTime = System.currentTimeMillis();
     }
 
-    public boolean writeDetails(String details) {
-        throw new IllegalStateException("there is no name written in this DeathNote or the details are null");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDeathCause(final String cause) {
+        if (cause == null) {
+            throw new IllegalStateException("the cause is null");
+        }
+        if (lastName == null) {
+            throw new IllegalStateException("there is no name written in this DeathNote");
+        }
+        if (System.currentTimeMillis() - lastNameTime > VALID_CAUSE_TIME) { //NOPMD
+            return false;
+        }
+        return deathnote.replace(lastName, lastDeath, new Death(cause, ""));
     }
 
-    public String getDeathCause(String name) {
-        throw new IllegalArgumentException("the provider name is not written in this DeathNote");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDetails(final String details) {
+        if (details == null) {
+            throw new IllegalStateException("the details are null");
+        }
+        if (lastName == null) {
+            throw new IllegalStateException("there is no name written in this DeathNote");
+        }
+        if (System.currentTimeMillis() - lastNameTime > VALID_DETAILS_TIME) { //NOPMD
+            return false;
+        }
+        return deathnote.replace(lastName, lastDeath, new Death("", details)); 
     }
 
-    public String getDeathDetails(String name) {
-        throw new IllegalArgumentException("the provider name is not written in this DeathNote");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathCause(final String name) {
+        if (!isNameWritten(name)) {
+            throw new IllegalArgumentException("the provider name is not written in this DeathNote");
+        }
+        return deathnote.get(name).cause;
     }
 
-    public boolean isNameWritten(String name) {
-        throw new NullPointerException("given name is null");
-        //Not sure about this one
-        //throw new UnsupportedOperationException("Not implemented yet"); ??
-        
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathDetails(final String name) {
+        if (!isNameWritten(name)) {
+            throw new IllegalArgumentException("the provider name is not written in this DeathNote");
+        }
+        return deathnote.get(name).details;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isNameWritten(final String name) {
+        Objects.requireNonNull(name, "name cannot be null");
+        return deathnote.containsKey(name);
+    }
+
+    private static final class Death {
+
+        private static final String DEFAULT_CAUSE = "heart attack";
+        private String cause;
+        private String details;
+
+        private Death(final String cause, final String details) {
+            this.cause = cause;
+            this.details = details;
+        }
+
+        private Death() {
+            this(DEFAULT_CAUSE, "");
+        }
     }
 
 }
---
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/api/TestDeathNote.java
@@ -0,0 +1,126 @@
+package it.unibo.deathnote.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static it.unibo.deathnote.api.DeathNote.RULES;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestDeathNote {
+
+  private static final String JIM_MORIARTY = "Jim Moriarty";
+  private static final String MURDER = "murder";
+  private static final String MYCROFT_HOLMES = "Mycroft Holmes";
+  private static final String SHERLOCK_HOLMES = "Sherlock Holmes";
+  private static final String DEFAULT_CAUSE = "heart attack";
+  private static final long INVALID_CAUSE_TIME = 100;
+  private static final long INVALID_DETAILS_TIME = 6000 + INVALID_CAUSE_TIME;
+
+  private DeathNote deathnote;
+
+  @BeforeEach
+  void setUp() {
+    deathnote = new DeathNoteImpl();
+  }
+
+  /*
+  1. Rule number 0 and negative rules do not exist in the DeathNote rules.
+    *check that the exceptions are thrown correctly, that their type is the expected one,
+      and that the message is not null, empty, or blank.
+  */
+  @Test
+  void testIllegalRule() {
+    for (final int index : List.of(-1, 0, RULES.size() + 1)) {
+      final Exception e = assertThrows(IllegalArgumentException.class, () -> {
+        deathnote.getRule(index);
+      }
+      );
+      assertNotNull(e.getMessage());
+      assertFalse(e.getMessage().isEmpty());
+      assertFalse(e.getMessage().isBlank());
+    }
+  }
+
+  /*
+  2. No rule is empty or null in the DeathNote rules.
+      * for all the valid rules, check that none is null or blank
+  */
+  @Test
+  void testValidRules() {
+    for (int i = 1; i < RULES.size(); i++) {
+      assertNotNull(RULES.get(i));
+      assertFalse(RULES.get(i).isBlank());
+    }
+  }
+
+  /*
+  3. The human whose name is written in the DeathNote will eventually die.
+      * verify that the human has not been written in the notebook yet
+      * write the human in the notebook
+      * verify that the human has been written in the notebook
+      * verify that another human has not been written in the notebook
+      * verify that the empty string has not been written in the notebook
+  */
+  @Test
+  void testDeath() {
+    assertFalse(deathnote.isNameWritten(JIM_MORIARTY));
+    deathnote.writeName(JIM_MORIARTY);
+    assertTrue(deathnote.isNameWritten(JIM_MORIARTY));
+    assertFalse(deathnote.isNameWritten(MYCROFT_HOLMES));
+    assertFalse(deathnote.isNameWritten(""));
+  }
+
+  /*
+  4. If the cause of death is written within the next 40 milliseconds of writing the person's name,
+    it will happen. 
+  */
+  @Test
+  void testDeathInTime() throws InterruptedException {
+    deathnote.writeName(MYCROFT_HOLMES);
+    assertEquals(DEFAULT_CAUSE, deathnote.getDeathCause(MYCROFT_HOLMES));
+    deathnote.writeName(JIM_MORIARTY);
+    assertTrue(deathnote.writeDeathCause(MURDER));
+    assertEquals(MURDER, deathnote.getDeathCause(JIM_MORIARTY));
+    Thread.sleep(INVALID_CAUSE_TIME);
+    assertFalse(deathnote.writeDeathCause("decapitation"));
+    assertEquals(MURDER, deathnote.getDeathCause(JIM_MORIARTY));
+  }
+
+  /*
+  5. After writing the cause of death, details of the death should be written
+     in the next 6 seconds and 40 milliseconds of writing the death's cause.
+      * check that writing the death details before writing a name throws the correct exception
+      * write the name of a human in the notebook
+      * verify that the details of the death are currently empty
+      * set the details of the death to "ran for too long"
+      * verify that death details have been set correctly 
+        (returned true, and the details are indeed "ran for too long")
+      * write the name of another human in the notebook
+      * sleep for 6100ms
+      * try to change the details
+      * verify that the details have not been changed
+  */
+  @Test
+  void testDetailsInTime() throws InterruptedException {
+    final Exception e = assertThrows(IllegalStateException.class, () -> {
+      deathnote.writeDetails("decapitation by a flying boomerang");
+    }
+    );
+    assertNotNull(e.getMessage());
+    assertFalse(e.getMessage().isEmpty());
+    assertFalse(e.getMessage().isBlank());
+    deathnote.writeName(MYCROFT_HOLMES);
+    assertTrue(deathnote.getDeathDetails(MYCROFT_HOLMES).isEmpty());
+    assertTrue(deathnote.writeDetails("ran for too long"));
+    assertEquals("ran for too long", deathnote.getDeathDetails(MYCROFT_HOLMES));
+    deathnote.writeName(SHERLOCK_HOLMES);
+    Thread.sleep(INVALID_DETAILS_TIME);
+    deathnote.writeDetails("decapitation by a flying boomerang");
+    assertEquals("", deathnote.getDeathDetails(SHERLOCK_HOLMES));
+  }
+}
